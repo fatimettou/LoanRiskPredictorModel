49400ecd79256851c8a6124ca7146b4bf17a7cbe
fatimettou <60854423+fatimettou@users.noreply.github.com>
2024-09-08 13:55:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def predict():
         return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
